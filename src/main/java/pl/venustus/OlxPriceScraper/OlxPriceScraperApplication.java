e3b105990052d222165e5e9535e80269004583e7
Tomek <venustus.pl@gmail.com>
2020-08-25 11:10:59 +0200

    0.0.1 First commit

---
+++ src/main/java/pl/venustus/OlxPriceScraper/OlxPriceScraperApplication.java
@@ -0,0 +1,13 @@
+package pl.venustus.OlxPriceScraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxPriceScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxPriceScraperApplication.class, args);
+	}
+
+}
